0f56f48ad6b38c1df132b8e76368e9ddb659e4d3
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-04-21 20:32:43 +0600

    added a responsive Navbar

---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostsPage = () => {
+    return (
+        <div>
+            <h2 className="font-bold text-3xl text-center">Posts are coming soon</h2>
+        </div>
+    );
+};
+
+export default PostsPage;
