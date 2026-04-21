de7828bc0563e986774c4e596bde01135062bd66
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-04-21 20:50:25 +0600

    data fetching in different ways

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 
-const PostsPage = () => {
+// const getPosts = async() => {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     const posts= res.json();
+//     return posts
+// }
+
+// const getPosts = async () => {
+//     try {
+//          const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     const posts= res.json();
+//     return posts
+//     }
+//     catch (err) {
+//         throw new Error('failed to fetch posts')
+//     }
+
+const getPosts = async () => {
+    
+         const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = res.json();
+    
+    if (!res.ok) {
+        throw new Error("failed to; load data")
+    }
+    return posts
+    
+   
+}
+
+const PostsPage = async() => {
+    // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    // const data = await res.json();
+
+    const data = await getPosts()
     return (
         <div>
-            <h2 className="font-bold text-3xl text-center">Posts are coming soon</h2>
+            <h2 className="font-bold text-3xl text-center">Posts are coming soon { data.length}</h2>
         </div>
     );
 };
